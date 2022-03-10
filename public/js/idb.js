4291f5d52130b0a02950e2233fc326913f4fa8a0
sm3131 <sm2683@nau.edu>
2022-03-10 15:34:58 -0700

    added indexedDB functionality to save data offline and post to DB when back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,8 +5,8 @@ const request = indexedDB.open('budget', 1);
 request.onupgradeneeded = function (event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+    // create an object store (table) called `new_money`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_money', { autoIncrement: true });
 };
 
 // upon a successful 
@@ -14,9 +14,9 @@ request.onsuccess = function (event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
 
-    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    // check if app is online, if yes run uploadMoney() function to send all local db data to api
     if (navigator.onLine) {
-        // uploadTransaction();
+        uploadMoney();
     }
 };
 
@@ -24,3 +24,59 @@ request.onerror = function (event) {
     // log error here
     console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new money and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_money'], 'readwrite');
+
+    // access the object store for `new_money`
+    const moneyObjectStore = transaction.objectStore('new_money');
+
+    // add record to your store with add method
+    moneyObjectStore.add(record);
+}
+
+function uploadMoney() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_money'], 'readwrite');
+
+    // access your object store
+    const moneyObjectStore = transaction.objectStore('new_money');
+
+    // get all records from store and set to a variable
+    const getAll = moneyObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_money'], 'readwrite');
+                    // access the new_money object store
+                    const moneyObjectStore = transaction.objectStore('new_money');
+                    // clear all items in your store
+                    moneyObjectStore.clear();
+
+                    alert('All saved money has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+window.addEventListener('online', uploadMoney);
